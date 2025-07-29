befc0bdfc07e61e285288a49817517e764b285ee
Daniel Engval <daniel@engvalls.eu>
2025-07-29 12:54:32 +0200

    Adjust off-time calculation in `blink.py` to scale with `max_off_sec`.

--- src/jamwatch/blink.py
+++ src/jamwatch/blink.py
@@ -46,7 +46,7 @@ class Blink:
         if self.perc_blink > 99:
             self.led.on()
             return
-        off_time_secs = ((100 - self.perc_blink) / 100 * max_off_sec)
+        off_time_secs = ((100 - self.perc_blink) / 100 * max_off_sec) * 10  # TODO: improve this logic
         self.led.blink(on_time=on_time_sec, off_time=off_time_secs, background=True)
         self.state = BlinkState.BLINK
         logger.info(f"Blinking LED on for {on_time_sec}s and off {off_time_secs}s")
